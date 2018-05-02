0a411c129af698505b530d40aa2ed445917c58e8
littleflute <13699175164@sina.cn>
2018-05-02 12:17:07 -0700

    Update i.js

--- Issues/1/i.js
+++ Issues/1/i.js
@@ -1,1 +1,11 @@
 
+var s = '<div class="header"><div class="header-right" id="id_div_title">'; s += '<a href= "https://github.com/littleflute/bluespiano/issues/1" target="_blank">#1_v0.0.  134</a> ';
+s += "<a class='xd1' target='_blank' href='https://github.com/littleflute/bluespiano/edit/master/Issues/1/i.js'"
+s +=">";		s += " i1.js*  ";		s += "</a> "; 
+		s += "  <a class='xd2'    target='_blank'  href='https://littleflute.github.io/bluespiano/Issues/1/i.js'";
+ 
+		s +=">";
+		s += " i1.js ";
+		s += "</a>"; 
+ s += '</div></div>';
+var d = blo0.blMDiv(document.body,"id1234",s,50,50,400,300,blGrey[0]);
