fff03a060be76124298154fbac81e3fa97d173b8
littleflute <13699175164@sina.cn>
2018-06-16 22:57:47 -0700

    Update i.js

--- Issues/1/i.js
+++ Issues/1/i.js
@@ -3,13 +3,16 @@ if(d) { b = bl$("id_btn_LoadIssue");_on_off_div(b,d);}
 else{
 
  var s = '<div class="header"><div class="header-right" id="id_div_title">'; 
-s += '<a href= "https://github.com/littleflute/bluespiano/issues/1" target="_blank">#1_v0.0.  241</a> ';
+s += '<a href= "https://github.com/littleflute/bluespiano/issues/1" target="_blank">#1_v0.0.  245</a> ';
 s += "<a class='xd1' target='_blank' href='https://github.com/littleflute/bluespiano/edit/master/Issues/1/i.js'"
 s +=">";		s += " i1.js*  ";		s += "</a> "; 
 		s += "  <a class='xd2'    target='_blank'  href='https://littleflute.github.io/bluespiano/Issues/1/i.js'"; 
 		s +=">";		s += " i1.js ";		s += "</a>";   
  s += '</div></div>';
 var d = blo0.blMDiv(document.body,"id1234",s,50,50,400,300,blGrey[0]);
+d.v1 = blo0.blDiv(d,d.id + "v1", "v1",blGrey[0]);
+d.v1.t1 = blo0.blTextarea (d.v1,d.v1.id + "t1","t1",blGrey[1]);
+
  b = bl$("id_btn_LoadIssue");_on_off_div(b,d);
 }
 
